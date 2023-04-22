db6edbb4f6082f89e08da9af23b16d7f8b6893a3
ruthpolly <>
2023-04-22 13:11:34 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,13 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+
 });
 
 
@@ -21,10 +24,17 @@ document.addEventListener("DOMContentLoaded", function () {
  * the main game 'loop', called when the script is first loaded
  * and after the users answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -44,7 +54,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
